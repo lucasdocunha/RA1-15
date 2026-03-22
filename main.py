86f116aa66e0b3d0b4886ed4221351e73a3d3117
Lucas <c.oliveira25@pucpr.edu.br>
2026-03-22 19:24:36 -0300

    Add: Primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+## Lucas de Oliveira Cunha - lucasdocunha
+## Tiago de Brito Follador - TiagoFollador
+## Grupo 15  
+
+def parseExpressao(linha:str, tokens:list[str]):
+    pass 
+
+def executarExpressao(dicionario:dict):
+    pass
+
+def gerarAssembly():
+    pass
+
+def exibirResultados():
+    pass 
+
